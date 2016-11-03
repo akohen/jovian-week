7923e83b35c5e97122c4a66306a896966bf882b1
Alexandre <alexandre@kohen.fr>
2016-11-03 17:41:50 +0100

    Changed port for Heroku

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.post('/test', function (req, res) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
