704292ff7182d81651c99eed633be5884b667af6
Alexandre <alexandre@kohen.fr>
2016-11-12 11:43:27 +0100

    Fix player echo bug

--- public/player.js
+++ public/player.js
@@ -5,7 +5,7 @@ game.player = {
   deltav: 100,
   balance: 156456000,
   hull: 100,
-  takeDamage: function(damage) { this.hull -= 10; this.term.echo('[[;red;]Took '+damage+' damage!]')},
+  takeDamage: function(damage) { this.hull -= 10; game.term.echo('[[;red;]Took '+damage+' damage!]')},
   canDock: function() { return (universe[game.player.location].type == "station")},
   dock: function() {},
   undock: function() {},
