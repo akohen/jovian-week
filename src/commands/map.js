e8779e498f9ab3a20f53f2988582281bd5cb7918
Alexandre <alexandre@kohen.fr>
2017-03-25 11:46:54 +0000

    Map command draws target and parent

--- src/commands/map.js
+++ src/commands/map.js
@@ -4,14 +4,19 @@ const location = require('../location.js')
 // Reference direction is always on top
 
 function drawBody(ctx, body, x, y, scale) {
-  let r = scale
+  let r = Math.max(5,scale * body.radius)
   ctx.strokeStyle = (body.color) ? body.color : "white"
   ctx.fillStyle = (body.color) ? body.color : "white"
   ctx.font = '18px monospace'
   ctx.beginPath()
   ctx.arc(x,y,r,0,2*Math.PI)
   ctx.stroke()
-  ctx.fillText(body.name, x+r, y-r)
+  var text = ctx.measureText(body.name)
+  if(text.width < 2*r) {
+    ctx.fillText(body.name, x-(text.width/2), y+5)
+  } else {
+    ctx.fillText(body.name, x+r, y-r)
+  }
 }
 
 // Draw an orbiting body
@@ -22,33 +27,54 @@ function drawOrbit(ctx, body, x, y, scale) {
   let a = body.sma * scale // semi-major axis
   let b = a*Math.sqrt(1-e*e) //semi-minor axis
   let r = body.argumentOfPeriapsis
+  let E = orbit.getEccentricAnomaly(body)
 
-  let centerX = x + scale * body.sma * body.eccentricity * Math.sin(body.argumentOfPeriapsis)
-  let centerY = y + scale * body.sma * body.eccentricity * Math.cos(body.argumentOfPeriapsis)
+
+  let centerX = x + scale * body.sma * body.eccentricity * Math.sin(r)
+  let centerY = y + scale * body.sma * body.eccentricity * Math.cos(r)
 
   ctx.strokeStyle = (body.color) ? body.color : "white"
   ctx.fillStyle = (body.color) ? body.color : "white"
 
-  console.log(`center: ${centerX},${centerY} axes:(${a},${b}) r:${r}`)
   ctx.beginPath()
   ctx.ellipse(centerX,centerY,b,a,-r,0,2*Math.PI,true)
   ctx.stroke()
-  ctx.fillText(body.name, centerX+a, centerY)
+
+
+  // getting the body position vector (with Pe on top)
+  let posX = - b * Math.sin(E)
+  let posY = - a * Math.cos(E)
+
+  // rotating the position vector (if we have a longitude of periapsis <> 0)
+  let nposX = posX * Math.cos(-r) - posY * Math.sin(-r)
+  let nposY = posX * Math.sin(-r) + posY * Math.cos(-r)
+
+  // translating
+  posX = nposX + centerX
+  posY = nposY + centerY
+
+  // Drawing the body at its current location
+  drawBody(ctx, body, posX, posY, scale)
 
 }
 
 // usage map target (default player)
 const command = {
   run: function(cmd) {
+    console.log(cmd)
+    var target = location.universe.player
+    if(location.universe[cmd]) target = location.universe[cmd]
+
     let img = new Image()
     var canvas = document.createElement('canvas');
-    canvas.width = 400;
+    canvas.width = 600;
     canvas.height = 400;
 
     // Get the drawing context
     var ctx = canvas.getContext('2d');
-    drawBody(ctx, location.universe.player.parent,200,200,60)
-    drawOrbit(ctx, location.universe.player,200,200,100/location.universe.player.sma)
+    var scale = 100/target.sma
+    drawBody(ctx, target.parent,200,200,scale)
+    drawOrbit(ctx, target,200,200,scale)
     return `<img src="${canvas.toDataURL('image/png')}">`
   },
 
