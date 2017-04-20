e81af47a5416433830ab2deb9344309e65472cd8
Alexandre <alexandre@kohen.fr>
2017-04-20 14:42:09 +0100

    Body class, maneuvers

--- src/utils/body.js
+++ src/utils/body.js
@@ -2,6 +2,7 @@
 
 const data = require('../data.js')
 const time = require('./time.js')
+const universe = require('../location.js').universe
 
 class Body {
   constructor(params) {
@@ -30,29 +31,35 @@ class Body {
   get i() { return this.inclination }
   get lop() { return this.longitudeOfAscendingNode + this.argumentOfPeriapsis }
   get n() { return Math.sqrt( this.parent.µ / Math.pow(this.a,3) ) } // Mean angular motion
-  get T() { return 2 * Math.PI * Math.sqrt( Math.pow(this.a,3)/this.parent.µ ) } // orbital period
+  get T() { return this.getPeriod() } // orbital period
   get t0() { return this.epoch }
-  get tAp() { return }
-  get tPe() { return }
+  get tAp() { return this.getTimeAtNextMeanAnomaly(Math.PI) }
+  get tPe() { return this.getTimeAtNextMeanAnomaly(2*Math.PI) }
   get time() { return (this.tRef) ? this.tRef : time.current } // time at which to compute all values
-  set time(t) { this.tRef = t }
+  set time(t) { this.tRef = t } // override current time as reference time for computations
+  resetTime() { delete this.tRef }
   get M() { return this.getMeanAnomaly() }
   get M0() { return this.anomalyAtEpoch }
   get E() { return this.getEccentricAnomaly() }
   get f() { return this.getTrueAnomaly() }
-  get Ap() { return }
-  get Pe() { return }
+  get Ap() { return getApoapsis() }
+  get Pe() { return getPeriapsis() }
   get r() { return this.a * ( 1 - this.e * Math.cos(this.E) ) }
   get h() { return this.r - this.parent.radius }
+  get hAp() { return this.Ap - this.parent.radius }
+  get hPe() { return this.Pe - this.parent.radius }
   get µ() { return 6.67408e-11 * this.mass }
   get v() { return this.getVelocity() }
 
 
   // Parent / children functions
-  setParent(parent) { 
-    if(this.parent) this.parent.removeChild(this);
+  // Set updateParent to false to leave parent bodies unchanged (to simulate but not apply a parent change for example)
+  setParent(parent, updateParent = true) { 
+    if(updateParent) {
+      if(this.parent) this.parent.removeChild(this);
+      parent.addChild(this)
+    }
     this.parent = parent
-    parent.addChild(this)
   }
 
   addChild(child) {
@@ -98,7 +105,8 @@ class Body {
 
 
   // Distance and altitude functions
-
+  getApoapsis() { return this.a * (1 - this.e) }
+  getPeriapsis() { return this.a * (1 + this.e) }
 
 
   // Velocity functions
@@ -106,33 +114,87 @@ class Body {
     return Math.sqrt( this.parent.µ * (2/this.r - 1/this.a) )
   }
 
+  getMeanVelocity() {
+    return Math.sqrt( this.parent.µ / this.a )
+  }
+
 
   // Time functions
   getPeriod() {
+    return 2 * Math.PI * Math.sqrt( Math.pow(this.a,3)/this.parent.µ );
+  }
+  getTimeAtNextMeanAnomaly(Mi) {
+    let ti = this.t0 + ( Mi - this.M0 ) / this.n
+    let timeleft = (ti -this.time) % this.T
+    return this.time + (timeleft + this.T) % this.T
+  }
 
+  getSynodicPeriod(body) {
+    let inversePeriod = 1/this.T - 1/body.T
+    return Math.abs(1/inversePeriod)
   }
 
 
   // Maneuver functions
   // this returns a temporary copy !
-  getStateAt(t=this.time) { // Returns the body with the maneuvers applied at t
-    return t
-  }
-
-  addManeuver(maneuver) {
-    if(!this.maneuvers) this.maneuvers = [];
-    if( maneuver.epoch > getFinalState().epoch ) this.maneuvers.push(maneuver);
+  getStateAt(t=this.time) { // Returns the body with the maneuvers applied at t, cannot go back in time
+    let copy = Object.assign({},this);
+    while(copy.maneuvers.length > 0 && copy.maneuvers[0].epoch < t) {
+      copy.doNextManeuver(false)
+    }
+    copy.time = t;
+    return copy
   }
 
   // get state once all the remaining maneuvers have been executed
   // this returns a temporary copy !
   getFinalState() { 
+    let copy = Object.assign({},this);
+    while(copy.maneuvers.length > 0) {
+      copy.doNextManeuver(false)
+    }
+    copy.time = copy.epoch;
+    return copy;
+  }
 
+  isNextManeuverDue() {
+    if(this.maneuvers.length == 0) return false
+    if(this.maneuvers[0].epoch > this.time) return false;
+    return true
+  }
+
+  doNextManeuver(modifyOthers = true) {
+    let maneuver = this.maneuvers[0]
+    if(maneuver) {
+      this.maneuvers.slice();
+      return this.doManeuver(maneuver, modifyOthers);
+    }
+    else return false;
   }
 
   // If modifyOthers = false, will not modify other objects
   doManeuver(maneuver, modifyOthers = true) {
+    const params = ['sma', 'anomalyAtEpoch', 'eccentricity', 'inclination', 'longitudeOfAscendingNode', 'argumentOfPeriapsis', 'epoch']
+    
 
+    for(let p of params) {
+      if(maneuver[p] !== undefined) this[p] = maneuver[p]
+    }
+
+    if(maneuver.parent && universe[maneuver.parent]) this.setParent(universe[maneuver.parent], modifyOthers);
+    
+    return true
+  }
+
+  addManeuver(maneuver) {
+    if(!this.maneuvers) this.maneuvers = [];
+    if( maneuver.epoch > this.maneuvers.peek().epoch ) this.maneuvers.push(maneuver);
+  }
+
+  update() {
+    while(this.isNextManeuverDue()) {
+      this.doNextManeuver()
+    }
   }
 
 
